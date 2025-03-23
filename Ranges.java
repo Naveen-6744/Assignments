30106f81f23a4579b9314a93afa39fc6d0578a0e
Naveen Kumar <nkkaran943@gmail.com>
2025-03-23 22:42:07 +0530

    Create Ranges.java

---
+++ Ranges.java
@@ -0,0 +1,23 @@
+//4. WAP to group list of numbers by their Ranges
+package com.naveen.asignments;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Ranges {
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(3, 7, 12, 25, 18, 33, 39, 44, 52, 67, 78, 85);
+
+        
+        Map<String, List<Integer>> groupedNumbers = numbers.stream()
+            .collect(Collectors.groupingBy(num -> {
+                int lower = (num / 10) * 10 + 1;
+                int upper = lower + 9;
+                return lower + "-" + upper;
+            }));
+
+        
+        groupedNumbers.forEach((range, numList) -> 
+            System.out.println("Range " + range + ": " + numList));
+    }
+}
